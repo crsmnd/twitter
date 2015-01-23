bb520e7999d13259184ec84fda1c06e74bf61e7e
Vincent Daranyi <vincent@daranyi.com>
2015-01-23 15:36:48 -0500

    A Basic Template

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
 
 // Initiate instance of Express
 var app = express();
@@ -7,8 +8,18 @@ var app = express();
 // Start up Morgan Middleware
 app.use(morgan('dev'));
 
+// Set default rendering setting to html
+app.set('view engine', 'html');
+// Use Swig Template Rendering Machine to render html
+app.engine('html', swig.renderFile);
+// Set views path to our views folder >> what does this do exactly?
+app.set('views', '/views');
+// Turn off cashing in Swig = constant refresh
+swig.setDefaults({ cache: false });
+
 // Handlers
 app.get("/", function(req, res) {
+	console.log(app.get('views'));
 	res.send("Hello World!");
 });
 
