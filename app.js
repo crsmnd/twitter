d4d40c8abcfd911321dc7f2e40f3b03597e547ed
Colin VanLang <colinvanlang@gmail.com>
2015-01-23 16:00:40 -0500

    tweetbank set up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var swig = require('swig');
+var tweetBank = require('./tweetBank');
 
 // Initiate instance of Express
 var app = express();
@@ -13,23 +14,25 @@ app.set('view engine', 'html');
 // Use Swig Template Rendering Machine to render html
 app.engine('html', swig.renderFile);
 // Set views path to our views folder >> what does this do exactly?
-app.set('views', '/views');
+app.set('views', './views');
 // Turn off cashing in Swig = constant refresh
 swig.setDefaults({ cache: false });
-
-// Handlers
-app.get("/", function(req, res) {
-	console.log(app.get('views'));
-	res.send("Hello World!");
-});
-
-app.get("/tweets", function(req, res) {
-	res.send("Now you are in the Tweet directory!");
-});
 
 
 // Activate server to listen at port 1234
 var port = 3000;
 app.listen(port, function() {
 	console.log("server listening");
+});
+
+
+
+// Handlers
+app.get("/", function(req, res) {
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
+app.get("/tweets", function(req, res) {
+	res.send("Now you are in the Tweet directory!");
 });
