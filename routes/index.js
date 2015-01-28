66f49a82ae84ef7afe8358f258f776f4500b6612
Colin VanLang <colinvanlang@gmail.com>
2015-01-28 18:21:28 -0500

    twitter.sql done

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,45 @@
 module.exports = function(app, io) {
+  var Sequelize_Objs = require('../models/');
+  var User = Sequelize_Objs.User;
+  var Tweet = Sequelize_Objs.Tweet;
 
-  // var express = require('express');
+
   var tweetBank = require('../tweetBank');
+  // var express = require('express');
   // var router = express.Router();
 
-
-  // var bodyParser = require('body-parser');
-  // router.use(bodyParser.json());
-
   app.get('/', function (req, res) {
-    console.log('hi');
-    var User = require('../models/').User;
-    User.find(1).complete(function(err, user) {
-        console.log('user obj:', user);
-        user.getTweets().complete(function(err, tweets) {
-            console.log('tweet console log', tweets[0].tweet);
-      })
+    Tweet.findAll({include: [User], order: 'id DESC'}).then(function(tweets) {
+      res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
     });
-
-
-    var tweets = tweetBank.list();
-    res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
   });
 
   app.get('/users/:name', function(req, res) {
     var name = req.params.name;
-    var list = tweetBank.find( {name: name} );
-    res.render( 'index', {
-    	title: 'Twitter.js - Posts by ' + name,
-    	tweets: list,
-    	showForm: true,
-    	user_page: name
-    } );
+    User.find({where: {name: name}}).then(function(user) {
+      user.getTweets({include: [User]}).then(function(tweets) {
+        res.render( 'index', {
+        	title: 'Twitter.js - Posts by ' + name,
+        	tweets: tweets,
+        	showForm: true,
+        	user_page: name
+        } );
+      });
+    });
   });
 
 
   app.post('/submit', function(req, res) {
-  	console.log(req.body);
   	var name = req.body.name;
   	var text = req.body.text;
-  	tweetBank.add(name, text);
-  	io.sockets.emit('new_tweet', { name: name, text: text });
-  	res.redirect('/');
-  })
+
+    User.findOrCreate({where: {name: name}}).then(function(user) {
+      Tweet.create({UserId: user[0].id, tweet: text}).then(function(tweet) {
+        io.sockets.emit('new_tweet', { name: name, text: text, id: user[0].id });
+        res.redirect('/');
+      });
+    });
+  });
 
 
 
